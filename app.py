f9d4fad25908c75dbb149aa4f06342469b0b94b0
AmirMS90 <amir.m.s1010@gmail.com>
2024-06-13 21:42:04 +0330

    Added the main page
    Added index.html that welcomes users

--- app.py
+++ app.py
@@ -2,3 +2,13 @@
     Usage of AI:
     Using codeium for commenting and autocomplete
 """
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
